a61975262d7b5ad9074f718dbb40b67e148a5233
qwerty2944 <qwerty.2944@gmail.com>
2024-07-24 00:32:36 +0900

    chore: asset 코드제네레이션

---
+++ lib/common/gen/fonts.gen.dart
@@ -0,0 +1,21 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: Adamina
+  static const String adamina = 'Adamina';
+
+  /// Font family: Texgyreheros
+  static const String texgyreheros = 'Texgyreheros';
+
+  /// Font family: Texgyreheroscn
+  static const String texgyreheroscn = 'Texgyreheroscn';
+}
