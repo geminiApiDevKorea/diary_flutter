fc0de9a656c318d003b7f856d4f461721ecf4364
qwerty2944 <qwerty.2944@gmail.com>
2024-08-02 02:29:19 +0900

    feat: Add DateHeaderDisplay widget for displaying date header in journal entries

---
+++ lib/presentation/journal/date_header_display.dart
@@ -0,0 +1,37 @@
+import 'package:diary_flutter/common/enums.dart';
+import 'package:diary_flutter/presentation/style/index.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart'; // Make sure to add this dependency in your pubspec.yaml
+
+class DateHeaderDisplay extends ConsumerWidget {
+  const DateHeaderDisplay({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final colors = ref.gemColors;
+    final textStyle = ref.gemTextStyle;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          DateTime.now().toMonthDayOrdinal(),
+          style: textStyle.h1.withColor(colors.grayScale0),
+        ),
+        const SizedBox(height: 12),
+        Text(
+          'From   ${DateTime.now().toDDMYYHH()}' '   Post',
+          style: textStyle.paragraph.withColor(colors.grayScale60),
+        ),
+        const SizedBox(height: 10),
+        Divider(
+          color: colors.grayScale80,
+          thickness: 1,
+        ),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+}
