eb499121a0801bc124a285198029d131a69415e6
qwerty2944 <qwerty.2944@gmail.com>
2024-08-02 02:28:03 +0900

    feat: 텍스트인풋용 프로바이더

---
+++ lib/presentation/journal/provider/post_text_input.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_text_input.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$postTextInputHash() => r'df3cafacad6030f81e0e5ad79dd7c42ef1b5e6f6';
+
+/// See also [PostTextInput].
+@ProviderFor(PostTextInput)
+final postTextInputProvider =
+    AutoDisposeNotifierProvider<PostTextInput, String?>.internal(
+  PostTextInput.new,
+  name: r'postTextInputProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$postTextInputHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PostTextInput = AutoDisposeNotifier<String?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
