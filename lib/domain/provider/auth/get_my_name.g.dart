01d5ab73d6248369a3a0255f7ed27223f12f9bc3
qwerty2944 <qwerty.2944@gmail.com>
2024-08-04 21:52:48 +0900

    feat: 유저이름얻기

---
+++ lib/domain/provider/auth/get_my_name.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_my_name.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getMyNameHash() => r'2ae6c7e7b95289c489de4543ba3e4a9c87216c19';
+
+/// See also [getMyName].
+@ProviderFor(getMyName)
+final getMyNameProvider = AutoDisposeProvider<String>.internal(
+  getMyName,
+  name: r'getMyNameProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$getMyNameHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetMyNameRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
