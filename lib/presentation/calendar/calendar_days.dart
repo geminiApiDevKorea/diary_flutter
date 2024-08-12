489817f943c21c6145da5b55ad62fd980c2a6f4b
aiiiiiiiden <aiiiiiiiden@gmail.com>
2024-08-12 21:27:36 +0900

    캘린더에서 음악 썸네일 클릭 시 일기 페이지 이동

--- lib/presentation/calendar/calendar_days.dart
+++ lib/presentation/calendar/calendar_days.dart
@@ -1,13 +1,35 @@
+import 'package:diary_flutter/common/enums.dart';
+import 'package:diary_flutter/data/repository/diary_repository.dart';
+import 'package:diary_flutter/domain/provider/common/focused_date.dart';
 import 'package:diary_flutter/domain/provider/diary/diary_use_cases.dart';
 import 'package:diary_flutter/presentation/calendar/calendar_music_day.dart';
 import 'package:diary_flutter/presentation/calendar/calendar_day.dart';
 import 'package:diary_flutter/presentation/calendar/calendar_inherited_notifier.dart';
 import 'package:diary_flutter/presentation/calendar/weekday_row_builder.dart';
+import 'package:diary_flutter/presentation/common/router.dart';
+import 'package:diary_flutter/presentation/journal/journal_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CalendarMusicDays extends ConsumerWidget {
   const CalendarMusicDays({super.key});
+
+  _onTap(BuildContext context, WidgetRef ref, MusicDiary diary) async {
+    await ref
+        .read(focusedDateProvider.notifier)
+        .updateDate(diary.dateTime)
+        .then(
+      (_) {
+        context.pushNamed(
+          JournalScreen.name,
+          queryParameters: {
+            QueryParameterKeys.feedbackType.value: diary.type.value,
+          },
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -34,7 +56,10 @@ class CalendarMusicDays extends ConsumerWidget {
                       .when(
                         data: (diary) => diary == null
                             ? CalendarDay(day: day)
-                            : CalendarMusicDay(musicDiary: diary),
+                            : CalendarMusicDay(
+                                musicDiary: diary,
+                                onTap: () => _onTap(context, ref, diary),
+                              ),
                         error: (error, stackTrace) => const SizedBox(width: 40),
                         loading: () => CalendarDay(day: day),
                       );
--- lib/presentation/calendar/calendar_music_day.dart
+++ lib/presentation/calendar/calendar_music_day.dart
@@ -4,24 +4,29 @@ import 'package:flutter/material.dart';
 
 class CalendarMusicDay extends StatelessWidget {
   final MusicDiary musicDiary;
+  final GestureTapCallback onTap;
   const CalendarMusicDay({
     super.key,
     required this.musicDiary,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: 30,
-        height: 30,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(15.0),
-          child: CachedNetworkImage(
-            imageUrl: musicDiary.musicThumbnailImageUrl,
-            fit: BoxFit.cover,
-            placeholder: (context, url) => const CircularProgressIndicator(),
-            errorWidget: (context, url, error) => const Icon(Icons.error),
+    return GestureDetector(
+      onTap: onTap,
+      child: Center(
+        child: SizedBox(
+          width: 30,
+          height: 30,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: CachedNetworkImage(
+              imageUrl: musicDiary.musicThumbnailImageUrl,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
           ),
         ),
       ),
